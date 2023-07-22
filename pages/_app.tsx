097d4e3d01271ee2f6ae4363e5409b51a286fd56
E2A4 <erkanaltinors10@gmail.com>
2023-07-22 15:04:10 +0300

    new nextjs setup

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Unbounded } from 'next/font/google'
+
+const unbounded = Unbounded({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['300','400', '600', '700'],
+  variable: '--font-unbounded'
+})
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <main className={`${unbounded.variable} font-sans`}><Component {...pageProps} /></main>
+}

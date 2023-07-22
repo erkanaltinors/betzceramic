ed76e91b519f9a03ee68479171f4fca9a6910afc
E2A4 <erkanaltinors10@gmail.com>
2023-07-23 00:37:04 +0300

    image next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/betzceramic'
+  images:{
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'erkanaltinors.github.io',
+        port: '',
+        pathname: '/betzceramic',
+      },
+    ],
+  }
 }
 
 module.exports = nextConfig
